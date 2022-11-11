b66b90ab31f5b64574dbc29dbd2e73d3728a50ac
Keshav Kumar <keshav.kumar@thoughtworks.com>
2022-11-12 02:58:12 +0530

    Add. Header & Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Header from './components/Header';
-
+import Footer from './components/Footer'; 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,82 @@
 import React from 'react'
-import './../static/css.css'
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import { Box } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#1976d2',
+    },
+  },
+});
+
 export default function Header() {
   return (
-    <div className='header'>
-        <div className='inline' >
-        <img className='verticalmiddle' id="logo" src={require('../static/img/logo.png')}></img>
-        <a className='verticalmiddle' href="#">Home</a>
-        <a className='verticalmiddle' href="#">Categories</a>
-        <a className='verticalmiddle' href="#">About Us</a>
-        </div>
-        <div className='inline rightHeaderItems'>
-        <a href="#">Signup</a>
-        </div>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+    <AppBar
+        position="static"
+        color="primary"
+        elevation={0}
+        sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+      >
+        <Toolbar sx={{ flexWrap: 'wrap' }}>
+          <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+          <Box
+        component="img"
+        sx={{
+          width: 170,
+          marginTop: 1,
+        }}
+        src={require('../static/img/logo.png')}
+      />
+          </Typography>
+          <nav>
+            <Link
+              variant="button"
+              underline="none"
+              color="text.primary"
+              href="#"
+              sx={{ my: 1, mx: 1.5 }}
+            >
+              Home
+            </Link>
+            <Link
+            underline="none"
+              variant="button"
+              color="text.primary"
+              href="#"
+              sx={{ my: 1, mx: 1.5 }}
+            >
+              Categories
+            </Link>
+            <Link
+              underline="none"
+              variant="button"
+              color="text.primary"
+              href="#"
+              sx={{ my: 1, mx: 1.5 }}
+            >
+              About Us
+            </Link>
+            <Link
+              underline="none"
+              variant="button"
+              href="#"
+              sx={{ my: 1, mx: 1.5 }}
+            >
+             Login
+            </Link>
+          </nav>
+        </Toolbar>
+      </AppBar>
+      </ThemeProvider>
+
   )
 }
